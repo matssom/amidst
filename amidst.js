af44f34e4d1951adc506fc743a0c8e6af00ba258
Mats Sommervold <mats.sommervold@gmail.com>
2020-09-19 21:05:58 +0200

    Added js file and test index.html file

---
+++ amidst.js
@@ -0,0 +1,15 @@
+document.head.innerHTML += '<link rel="stylesheet" href="amidst.css">';
+
+/*
+    Adding user-is-tabbing class to the body if the user starts tabbing.
+    This way we can know when to show outlines on for keyboard-users.
+*/
+
+const handleFirstTab = (e) => {
+    if (e.keyCode === 9) {
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+
+window.addEventListener('keydown', handleFirstTab);
