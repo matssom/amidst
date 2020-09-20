76e4273790bef648eb812d59fa91114f029043ec
mats1105 <mats.sommervold@gmail.com>
2020-09-20 13:19:02 +0200

    Refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  	entry: './development/src/index.js',
+  	entry: './src/index.js',
   	output: {
     	filename: 'amidst.js',
     	path: path.resolve(__dirname, 'test'),
 	},
-	watch: true,
-	optimization: {
-		runtimeChunk: true,
-	}
+	watch: true
 };
