205bf38f2782accf8add2bfb7862b6f733855dcc
Mats Sommervold <mats.sommervold@gmail.com>
2020-10-31 04:11:56 +0100

    Added bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,44 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCss = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OnBuild = require('on-build-webpack');
+const fs = require('fs');
 
-module.exports = {
-  	entry: './src/index.js',
+// const minifyCss = {
+// 	entry: './src/style.css',
+//   	output: {
+//     	path: path.resolve(__dirname, './'),
+// 	},
+// 	watch: true,
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.css$/i,
+// 				use: [
+// 					MiniCssExtractPlugin.loader,
+// 					'css-loader'
+// 				],
+// 			},
+// 		],
+// 	},
+// 	plugins: [
+// 		new MiniCssExtractPlugin({ filename: 'amidst.css' }),
+// 		new OnBuild(function () {
+// 			fs.unlinkSync(path.join(path.resolve(__dirname, './'), 'main.js'));
+// 		}),
+// 	],
+// 	optimization: {
+// 		minimizer: [
+// 			new OptimizeCss(),
+// 			new TerserPlugin()
+// 		]
+// 	},
+// 	mode: 'production'
+// }
+
+const minifyJs = {
+	entry: './src/index.js',
   	output: {
     	filename: 'amidst.js',
     	path: path.resolve(__dirname, './'),
@@ -11,9 +48,23 @@ module.exports = {
 		rules: [
 			{
 				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				],
 			},
 		],
 	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: 'amidst.css' })
+	],
+	optimization: {
+		minimizer: [
+			new OptimizeCss(),
+			new TerserPlugin()
+		]
+	},
 	mode: 'production'
-};
+}
+
+module.exports = [/* minifyCss, */ minifyJs];
